9010aa813e874d5aecfd4c6357729236cd1653eb
ZHAO YU <wuhulalu@gmail.com>
2026-05-16 19:09:33 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './app/App'
+import './styles/index.css'
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+const root = document.getElementById('root')
+
+if (root) {
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  )
+}
