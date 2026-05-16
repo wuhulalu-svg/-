416b2807034302d7496b8f041c87fbac6949a7a0
ZHAO YU <wuhulalu@gmail.com>
2026-05-16 19:02:14 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS ? '/' : '/',
+  base: process.env.GITHUB_ACTIONS ? '/-/',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
